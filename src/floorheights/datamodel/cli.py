6f9aaa77685d645343d04a247857c27826af1e6e
lachlan-ng <lng@frontiersi.com.au>
2024-11-01 15:57:03 +1100

    Added CLI command for ingesting building footprints
    Currently gets height_ahd by sampling an input DEM for each building centroid

--- src/floorheights/datamodel/cli.py
+++ src/floorheights/datamodel/cli.py
@@ -1,5 +1,7 @@
 import click
 import geopandas as gpd
+import rasterio
+from shapely.geometry import box
 from sqlalchemy.orm import Session
 from floorheights.datamodel.models import AddressPoint, Building, SessionLocal
 
@@ -73,9 +75,59 @@ def ingest_address_points(infile, chunksize):
     click.echo("Address ingestion complete")
 
 
+@click.command()
+@click.option("-i", "--infile", "infile", required=True, type=click.File(), help="Input building footprint (GeoParquet) file path.")
+@click.option("-d", "--dem", "dem", required=True, type=click.File(), help="Input DEM file path.")
+@click.option("-c", "--chunksize", "chunksize", type=int, default=None, help="Specify the number of rows in each batch to be written at a time. By default, all rows will be written at once.")
+def ingest_buildings(infile, dem, chunksize):
+    """Ingest building footprints"""
+    session = SessionLocal()
+    engine = session.get_bind()
+
+    click.echo("Loading DEM...")
+    dem = rasterio.open(dem.name)
+    dem_crs = dem.crs
+
+    click.echo("Creating mask...")
+    bounds = dem.bounds
+    mask_geom = box(*bounds)
+
+    mask_df = gpd.GeoDataFrame({"id":1,"geometry":[mask_geom]}, crs=dem_crs.to_string())
+    mask_df = mask_df.to_crs(4326)  # Transform mask to WGS84 - might be slightly offset buildings are in GDA94/GDA2020
+    mask_bbox = mask_df.total_bounds
+
+    click.echo("Loading building GeoParquet...")
+    buildings = gpd.read_parquet(infile.name, columns=["geometry"], bbox=mask_bbox)
+
+    buildings = buildings[buildings.geom_type == 'Polygon']  # Remove multipolygons
+    
+    click.echo("Sampling DEM with buildings...")
+    buildings = buildings.to_crs(dem_crs.to_epsg())  # Transform buildings to CRS of our DEM
+    centroids = [(x, y) for x, y in zip(buildings["geometry"].centroid.x, buildings["geometry"].centroid.y)]
+
+    buildings["height_ahd"] = [x[0] for x in dem.sample(centroids)]
+
+    buildings = buildings[buildings['height_ahd'] != dem.nodata]  # Remove any buildings sampled with no data
+    buildings = buildings.to_crs(4326)  # Transform back to WGS84
+    buildings = buildings.rename_geometry("outline")
+
+    click.echo("Copying to PostgreSQL...")
+    buildings.to_postgis(
+        "building",
+        engine,
+        schema="public",
+        if_exists="append",
+        index=False,
+        chunksize=chunksize,
+    )
+
+    click.echo("Building ingestion complete")
+
+
 cli.add_command(create_dummy_address_point)
 cli.add_command(create_dummy_building)
 cli.add_command(ingest_address_points)
+cli.add_command(ingest_buildings)
 
 
 if __name__ == '__main__':
